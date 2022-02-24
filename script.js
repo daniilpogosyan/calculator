b9c2f30b59c98bf1e76215c474caca7c0e31f124
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-02-24 19:23:30 +0500

    Add logic of expression computing
    Use `expression` to store current operands and operator.
    Use `expression.numberEntered` to separate two operands and
    clear display before input next operand.
    Add `data-key` to store key values due to incompatible with text of
    divide and multiply buttons

--- script.js
+++ script.js
@@ -8,22 +8,74 @@ const dot = document.getElementById('dot');
 const operators = document.getElementsByClassName('operator');
 const equal = document.getElementById('equal');
 
+const expression = {
+  num1: undefined,
+  num2: undefined,
+  operator: undefined,
+  // used to clear display after getting operator
+  numberEntered: false,
+
+  operate() {
+    switch (this.operator) {
+      case '+':
+        return +this.num1 + +this.num2;
+      case '-':
+        return this.num1 - this.num2;
+      case '*':
+        return this.num1 * this.num2;
+      case '/':
+        return this.num1 / this.num2;
+    }
+  },
+};
+
 clearAll.addEventListener('click', () => {
   display.textContent = '0';
+  expression.numberEntered = false;
+  expression.num1 = undefined;
+  expression.num2 = undefined;
+  expression.operator = undefined;
 });
+
 backspace.addEventListener('click', () => {
     display.textContent = display.textContent.slice(0,-1);
     if (display.textContent.length == 0) {
       display.textContent = '0';
+      expression.numberEntered = false;
     }
 });
 
 [...numbers].forEach(number => number.addEventListener('click', () => {
-  if (display.textContent == '0') {
-    display.textContent = number.textContent;
-  } else {
-    display.textContent += number.textContent;
+  if (!expression.numberEntered) {
+    display.textContent = '';
+    expression.numberEntered = true;
   }
+    display.textContent += number.dataset.key;
 }));
 
+[...operators].forEach(operator => operator.addEventListener('click', () => {
+  if (expression.num1 == undefined && expression.numberEntered) {
+    expression.num1 = display.textContent;
+  } else if (expression.num2 == undefined && expression.numberEntered) {
+    expression.num2 = display.textContent;
+    display.textContent = expression.operate();
 
+    expression.num1 = expression.operate();
+    expression.num2 = undefined;
+  }
+  expression.operator = operator.dataset.key;
+  expression.numberEntered = false;
+}));
+
+equal.addEventListener('click', () => {
+  if (expression.num1 != undefined &&
+      expression.operator != undefined) {
+    expression.num2 = display.textContent;
+    display.textContent = expression.operate();
+
+    expression.num1 = expression.operate();
+    expression.num2 = undefined;
+    expression.operator = undefined;
+    expression.numberEntered = false;
+  }
+});
