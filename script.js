57568edd9976cf46df9b15166104c8c6b33d22c4
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-02-26 17:33:21 +0500

    Add expresion logic
    Algorithm of expression calculating:
    1) Get the expression
    2) Break down the expression and prioritize operators
    3) Update expression calculating the highest priority operator
    4) Repeat 3) until the expression is calculated

--- script.js
+++ script.js
@@ -2,100 +2,147 @@ const display = document.getElementById('display');
 
 const clearAll = document.getElementById('clear-all');
 const backspace = document.getElementById('backspace');
+const parenthesis = document.getElementsByClassName('parenthesis');
 
 const numbers = document.getElementsByClassName('number');
 const dot = document.getElementById('dot');
 const operators = document.getElementsByClassName('operator');
 const equal = document.getElementById('equal');
 
-const expression = {
-  num1: undefined,
-  num2: undefined,
-  operator: undefined,
-  // used to clear display after getting operator
-  numberEntered: false,
-
-  precision: 8,
-  operate() {
-    let result;
-    switch (this.operator) {
-      case '+':
-        result =  +this.num1 + +this.num2;
-        break;
-      case '-':
-        result =  this.num1 - this.num2;
-        break;
-      case '*':
-        result =  this.num1 * this.num2;
-        break;
-      case '/':
-        result =  this.num1 / this.num2;
-        break;
-    }
-    return Math.round(result * Math.pow(10,this.precision)) / Math.pow(10,this.precision);
-  },
-};
 
 clearAll.addEventListener('click', () => {
   display.textContent = '0';
-  expression.numberEntered = false;
-  expression.num1 = undefined;
-  expression.num2 = undefined;
-  expression.operator = undefined;
 });
 
 backspace.addEventListener('click', () => {
     display.textContent = display.textContent.slice(0,-1);
-    if (display.textContent.length == 0 || !expression.operator) {
+    if (display.textContent.length == 0) {
       display.textContent = '0';
-      expression.numberEntered = false;
     }
 });
 
-[...numbers].forEach(number => number.addEventListener('click', () => {
-  if (!expression.numberEntered) {
+[...numbers, ...parenthesis].forEach(number => number.addEventListener('click', () => {
+  if (display.textContent == '0') {
     display.textContent = '';
-    expression.numberEntered = true;
-  }
-  if (expression.operator == undefined) {
-    expression.num1 = undefined;
   }
     display.textContent += number.dataset.key;
 }));
 
 [...operators].forEach(operator => operator.addEventListener('click', () => {
-  if (expression.num1 == undefined && expression.numberEntered) {
-    expression.num1 = display.textContent;
-  } else if (expression.num2 == undefined && expression.numberEntered) {
-    expression.num2 = display.textContent;
-    display.textContent = expression.operate();
-
-    expression.num1 = expression.operate();
-    expression.num2 = undefined;
-  }
-  expression.operator = operator.dataset.key;
-  expression.numberEntered = false;
+    display.textContent += operator.textContent;
 }));
 
 equal.addEventListener('click', () => {
-  if (expression.num1 != undefined &&
-      expression.operator != undefined) {
-    expression.num2 = display.textContent;
-    display.textContent = expression.operate();
-
-    expression.num1 = expression.operate();
-    expression.num2 = undefined;
-    expression.operator = undefined;
-    expression.numberEntered = false;
-  }
+  const expression = display.textContent;
+  display.textContent = computeExp(expression);
 });
 
 dot.addEventListener('click', () => {
-  if(!expression.numberEntered) {
-    display.textContent = '0';
-  }
-  if (!display.textContent.includes('.')) {
-    expression.numberEntered = true;
+  //if expression doesn't has no operators 
+  //then start search from the expression beginning 
+  let lastOpIndex = [...display.textContent.matchAll(/[\+\-\×\÷]/g)].pop()?.index || 0;
+
+  // if (!display.textContent.includes('.', lastOpIndex))
+  if (/(?<![\+\-\×\÷])$/.test(display.textContent.slice(lastOpIndex))
+      && !display.textContent.includes('.', lastOpIndex))
     display.textContent += '.';
-  }
 });
+
+
+function isValid(expression) {
+  const regExps = {
+    twoConsecutiveOps: /[\+\-\×\÷]{2,}/,
+    startParenthWithOp: /[\+\×\÷]\)/,
+    endParenthWithOp: /\([\+\×\÷]/,
+    startExpWithOp: /^[\+\×\÷]/,
+    emptyParenth: /\(\)/,
+  }
+
+  let parenthCounter = 0;
+  for(let char of expression.split('')) {
+    if (char  == '(')     parenthCounter++;
+    else if (char == ')') parenthCounter--;
+
+    if (parenthCounter < 0) return false;
+  }
+
+  if (parenthCounter !== 0) return false;
+
+  for (const rule in regExps) {
+    if (regExps[rule].test(expression)) return false;
+  }
+
+
+  return true;
+}
+
+function computeExp(expression) {
+
+  if (!isValid(expression)) return 'ERROR';
+
+  //to process expression like `-4+3...' correctly
+  //otherwise the first minus would be considered an operator between two operands
+  if (expression[0] == '-') 
+    expression = '0' + expression;
+
+  //ignore the last operator
+  if (/[\+\-\×\÷]/.test(expression[expression.length - 1]))
+    expression = expression.slice(0,-1);
+
+  const expArr = expression.match(/[^\d()]+|[\(\)]|[\d.]+/g);
+
+  let expPriority = [];
+  let parenthCounter = 0;
+  for (const piece of expArr) {
+      switch (piece) {
+        case '(':
+          parenthCounter++;
+          break;
+        case ')':
+          parenthCounter--;
+          break; 
+        case String(+piece):
+          expPriority.push(piece);
+          break
+        case '+':
+        case '-':
+        case '×':
+        case '÷':
+          const priority = ((piece == '+' || piece == '-')? 1 : 2) + parenthCounter * 3;
+          let op = {
+            operator: piece,
+            priority: priority
+          }
+          expPriority.push(op);
+      }
+  }
+
+  let j = 3
+  while(expPriority.length > 1) {
+    let operatorIndex;
+    let maxPriority = 0;
+    for (let i = 0; i < expPriority.length; i++) {
+      if (expPriority[i].priority > maxPriority) {
+        maxPriority = expPriority[i].priority;
+        operatorIndex = i;
+      }
+    }
+  
+    //change the highest priority operation into result of that operation
+    expPriority.splice(operatorIndex - 1, 3, 
+      operate(expPriority[operatorIndex - 1],
+        expPriority[operatorIndex + 1],
+        expPriority[operatorIndex].operator)); 
+  }
+  return expPriority[0];
+}
+
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case '+': return +num1 + +num2;
+    case '-': return num1 - num2;
+    case '×': return num1 * num2;
+    case '÷': return num1 / num2;
+  }
+}
